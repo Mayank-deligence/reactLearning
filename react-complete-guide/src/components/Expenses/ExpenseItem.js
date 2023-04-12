dfea429ee7cb316d6b6dbb027c2c026be16992b4
DELIGENCE <deligence@DELIGENCEs-MacBook-Pro.local>
2023-04-12 10:59:14 +0530

    initial react project started

---
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // const [title,setTitle]= useState(props.title);
+  //   // let title=props.title;
+  //   console.log("Reloaded item");
+  // const clickHandler = ()=>{
+  //   // console.log('clicked!!!')
+  //   // title='Updated!';
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // };
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
