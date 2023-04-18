c1fcebb4161a434e1780589a6b6ae0085be60395
DELIGENCE <deligence@DELIGENCEs-MacBook-Pro.local>
2023-04-18 13:07:23 +0530

    adding http and forms to food order app

---
+++ project 12/src/components/Cart/Checkout.js
@@ -0,0 +1,102 @@
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid &&
+      enteredStreetIsValid;
+
+    if (!formIsValid) {
+      return
+    }
+    props.onConfirm({
+        name:enteredName,
+        street:enteredStreet,
+        postalCode:enteredPostalCode,
+        city:enteredCity
+    });
+  };
+
+  const nameContolClasses = `${classes.control} ${
+    formInputValidity.name ? "" : classes.invalid
+  }`;
+  const streetContolClasses = `${classes.control} ${
+    formInputValidity.street ? "" : classes.invalid
+  }`;
+  const postalCodeContolClasses = `${classes.control} ${
+    formInputValidity.postalCode ? "" : classes.invalid
+  }`;
+  const cityContolClasses = `${classes.control} ${
+    formInputValidity.city ? "" : classes.invalid
+  }`;
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameContolClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputValidity.name && <p>Please enter a valid Name!</p>}
+      </div>
+      <div className={streetContolClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formInputValidity.street && <p>Please enter a valid Street!</p>}
+      </div>
+      <div className={postalCodeContolClasses}>
+        <label htmlFor="postal">Postal code</label>
+        <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputValidity.postalCode && (
+          <p>Please enter a valid Postal Code!</p>
+        )}
+      </div>
+      <div className={cityContolClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputValidity.city && <p>Please enter a valid City!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
