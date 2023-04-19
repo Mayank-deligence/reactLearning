c6f04541b48143b01f6f702beb60ee7db6052636
DELIGENCE <deligence@DELIGENCEs-MacBook-Pro.local>
2023-04-19 10:25:48 +0530

    Redux basics

---
+++ project 14 /src/store/index.js
@@ -0,0 +1,50 @@
+// import { legacy_createStore as createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import authReducer from "./authSlice";
+import counterReducer from "./counterSlice";
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+
+export default store;
+
+
+
+
+
+
+
+
+
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+//   return state;
+// };
